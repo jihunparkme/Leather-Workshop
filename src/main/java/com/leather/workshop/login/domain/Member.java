a752e87c17184cc75807230fc928577f07cff07f
jihun.park <jihunpark.tech@gmail.com>
2022-01-22 20:40:40 +0900

    [Add] 로그인 컨트롤러 추가

---
+++ src/main/java/com/leather/workshop/login/domain/Member.java
@@ -0,0 +1,11 @@
+package com.leather.workshop.login.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member {
+}
