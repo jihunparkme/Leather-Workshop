e438b19c2ab9b6f00293f9ce29d7a3ce457e73b1
jihun.park <jihunpark.tech@gmail.com>
2022-01-23 20:53:57 +0900

    [Add] Notice Entity, Repository 추가

---
+++ src/main/java/com/leather/workshop/notice/domain/NoticeRepository.java
@@ -0,0 +1,6 @@
+package com.leather.workshop.notice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
