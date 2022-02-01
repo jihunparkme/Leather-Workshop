99f142e20969778f9b296a48e374555bcdf11320
jihun.park <jihunpark.tech@gmail.com>
2022-02-01 15:37:46 +0900

    [EDIT] 공지사항 수정 페이지 개발

--- src/main/java/com/leather/workshop/domain/notice/web/NoticeController.java
+++ src/main/java/com/leather/workshop/domain/notice/web/NoticeController.java
@@ -2,11 +2,14 @@ package com.leather.workshop.domain.notice.web;
 
 import com.leather.workshop.domain.notice.domain.Notice;
 import com.leather.workshop.domain.notice.service.NoticeService;
+import com.leather.workshop.domain.notice.web.dto.request.NoticeUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -37,13 +40,32 @@ public class NoticeController {
         return "notice/notice-view";
     }
 
-//    @PostMapping("/save")
-//    public String save(@RequestBody NoticeSaveRequest noticeSaveRequest,
-//                     HttpServletRequest request, HttpServletResponse response) {
-//
-//        log.info("save form notice");
-//        Long id = noticeService.save(noticeSaveRequest);
-//
-//        return "redirect:/notice/notice-view/" + id;
-//    }
+    @GetMapping("/add")
+    public String add(Model model) {
+
+        model.addAttribute("notice", new Notice());
+        return "notice/notice-add";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+
+        model.addAttribute("notice", noticeService.findById(id));
+        return "notice/notice-edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String edit(@PathVariable Long id,
+                       @Validated @ModelAttribute("notice") NoticeUpdateRequest form,
+                       BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "notice/notice-edit";
+        }
+
+        noticeService.update(id, form);
+
+        return "redirect:/notice/" + id;
+    }
 }
--- src/main/java/com/leather/workshop/domain/notice/web/dto/request/NoticeUpdateRequest.java
+++ src/main/java/com/leather/workshop/domain/notice/web/dto/request/NoticeUpdateRequest.java
@@ -4,11 +4,16 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 public class NoticeUpdateRequest {
 
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String contents;
 
     @Builder
