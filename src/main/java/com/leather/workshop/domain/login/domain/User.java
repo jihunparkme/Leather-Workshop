b1135e5ad96e7f7a2629d2b21742ed199acf1bcd
jihun.park <jihunpark.tech@gmail.com>
2022-02-03 21:54:53 +0900

    [ADD] User 엔티티 관련 코드 추가

---
+++ src/main/java/com/leather/workshop/domain/login/domain/User.java
@@ -0,0 +1,30 @@
+package com.leather.workshop.domain.login.domain;
+
+import com.leather.workshop.global.common.domain.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
