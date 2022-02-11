beb6a9938ea0665fad71acb5c7c50d489eecbd0e
Jihun Park <jihunpark.tech@gmail.com>
2022-02-11 23:08:59 +0900

    [Edit] spring config file type 변경 (properties -> yml)

--- src/main/java/com/leather/workshop/global/config/ProductionConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.leather.workshop.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Profile("test")
-@Configuration
-public class ProductionConfiguration {
-}
