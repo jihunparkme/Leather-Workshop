750aa620a5d71d937a7efa69bf9f93982e8850f5
Jihun Park <jihunpark.tech@gmail.com>
2022-02-15 09:57:30 +0900

    [Edit] 테이블 쿼리 수정

--- src/main/resources/db/sql/schema-h2.sql
+++ src/main/resources/db/sql/schema-h2.sql
@@ -21,3 +21,28 @@ ALTER TABLE `Notice`
 
 ALTER TABLE `Notice`
     AUTO_INCREMENT = 1;
+
+
+-- 사용자
+CREATE TABLE `USER` (
+                        `id`                 BIGINT       NOT NULL, -- 사용자 ID
+                        `name`               VARCHAR(30)  NOT NULL, -- 이름
+                        `email`              VARCHAR(50)  NOT NULL, -- email
+                        `picture`            VARCHAR(255)  NOT NULL, -- picture
+                        `role`               VARCHAR(10)  NOT NULL, -- 권한
+                        `created_date_time`  DATETIME     NOT NULL, -- 등록일
+                        `modified_date_time` DATETIME     NULL      -- 수정일
+);
+
+-- 사용자
+ALTER TABLE `USER`
+    ADD CONSTRAINT `PK_USER` -- 사용자 기본키
+        PRIMARY KEY (
+                     `id` -- 사용자 ID
+            );
+
+ALTER TABLE `USER`
+    MODIFY COLUMN `id` BIGINT NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `USER`
+    AUTO_INCREMENT = 1;
