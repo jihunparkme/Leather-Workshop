ef1886aae457062610f502127d55ca3770a4dbbf
Jihun Park <jihunpark.tech@gmail.com>
2022-02-15 23:41:31 +0900

    [Edit] 네이버 OAuth 로그인 기능 추가

--- src/main/resources/static/js/login/login.js
+++ src/main/resources/static/js/login/login.js
@@ -2,8 +2,16 @@ $(function() {
     $('.google').click(function () {
        loginGoogle();
     });
+
+    $('.naver').click(function () {
+        loginNaver();
+    });
 });
 
 function loginGoogle() {
     location.href = "/oauth2/authorization/google"
+}
+
+function loginNaver() {
+    location.href = "/oauth2/authorization/naver"
 }
