0e8c804aefd400016509e2d86f439bca5f6ebd84
Jihun Park <jihunpark.tech@gmail.com>
2022-02-18 00:15:36 +0900

    [Edit] 내정보 페이지 개발

--- src/main/java/com/leather/workshop/domain/login/web/LoginController.java
+++ src/main/java/com/leather/workshop/domain/login/web/LoginController.java
@@ -1,7 +1,10 @@
 package com.leather.workshop.domain.login.web;
 
+import com.leather.workshop.global.config.security.LoginUser;
+import com.leather.workshop.global.config.security.dto.SessionUser;
 import com.leather.workshop.global.config.session.SessionConst;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,5 +16,11 @@ public class LoginController {
     public String login(HttpServletRequest request) {
         request.getSession().setAttribute(SessionConst.LOGIN_PREV_PAGE, request.getHeader("Referer"));
         return "login/login";
+    }
+
+    @GetMapping("/mypage")
+    public String myPage(@LoginUser SessionUser user, Model model) {
+        model.addAttribute("user", user);
+        return "login/mypage";
     }
 }
