ca8f4385dfe4b931415b45233250bf979dc88c8f
Jihun Park <jihunpark.tech@gmail.com>
2022-03-06 00:23:14 +0900

    [Edit] adResourceHandlers 설정 추가

--- src/main/java/com/leather/workshop/global/config/WebConfig.java
+++ src/main/java/com/leather/workshop/global/config/WebConfig.java
@@ -2,8 +2,10 @@ package com.leather.workshop.global.config;
 
 import com.leather.workshop.global.config.security.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,10 +13,22 @@ import java.util.List;
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    private String connectPath = "/img/**";
+
+    @Value("${file.directory}")
+    private String resourcePath;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(connectPath)
+                .addResourceLocations("file:///" + resourcePath);
     }
 }
