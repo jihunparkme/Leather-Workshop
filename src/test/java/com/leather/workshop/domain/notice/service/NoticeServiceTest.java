f875555aa859d47ac2613bafd0183521ad0a3248
Jihun Park <jihunpark.tech@gmail.com>
2022-03-28 15:24:20 +0900

    edit: notice service layer test code refactoring

--- src/test/java/com/leather/workshop/domain/notice/service/NoticeServiceTest.java
+++ src/test/java/com/leather/workshop/domain/notice/service/NoticeServiceTest.java
@@ -10,11 +10,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class NoticeServiceTest {
@@ -34,18 +38,6 @@ class NoticeServiceTest {
     String updateTitle = "제목 수정";
     String updateContents = "내용 수정";
 
-    private NoticeDto.SaveRequest request = NoticeDto.SaveRequest.builder()
-            .userId(userId)
-            .title(title)
-            .contents(contents)
-            .hits(hits).build();
-
-    private NoticeDto.UpdateRequest updateRequest = NoticeDto.UpdateRequest.builder()
-            .id(id)
-            .title(updateTitle)
-            .contents(updateContents)
-            .build();
-
     private Notice notice = Notice.builder()
             .id(id)
             .userId(userId)
@@ -54,34 +46,38 @@ class NoticeServiceTest {
             .hits(hits)
             .modifiedDateTime(null).build();
 
-    private Notice updateNotice = Notice.builder()
-            .id(id)
-            .userId(userId)
-            .title(updateTitle)
-            .contents(updateContents)
-            .hits(hits)
-            .modifiedDateTime(null).build();
-
     @Test
     @DisplayName("공지사항 등록 성공")
     void save() {
 
+        NoticeDto.SaveRequest request = NoticeDto.SaveRequest.builder()
+                .userId(userId)
+                .title(title)
+                .contents(contents)
+                .hits(hits).build();
+
         lenient().when(noticeRepository.save((any()))).thenReturn(Optional.of(notice).get());
 
-        Long save = noticeService.save(request);
+        noticeService.save(request);
 
-        assertThat(save).isEqualTo(id);
+        verify(noticeRepository).save(any());
     }
 
     @Test
     @DisplayName("공지사항 수정 성공")
     void update() {
+
+        NoticeDto.UpdateRequest updateRequest = NoticeDto.UpdateRequest.builder()
+                .id(id)
+                .title(updateTitle)
+                .contents(updateContents)
+                .build();
+
         lenient().when(noticeRepository.findById(any())).thenReturn(Optional.of(notice));
-        notice.update(updateTitle, updateContents);
 
-        Long update = noticeService.update(id, updateRequest);
+        noticeService.update(id, updateRequest);
 
-        assertThat(update).isEqualTo(id);
+        verify(noticeRepository).findById(any());
     }
 
     @Test
@@ -90,9 +86,13 @@ class NoticeServiceTest {
         lenient().when(noticeRepository.findById(any())).thenReturn(Optional.of(notice));
 
         noticeService.delete(id);
+
+        verify(noticeRepository).findById(any());
+        verify(noticeRepository).delete(any());
     }
 
     @Test
+    @DisplayName("공지사항 조회 성공")
     void findById() {
         lenient().when(noticeRepository.findById(any())).thenReturn(Optional.of(notice));
 
@@ -103,5 +103,17 @@ class NoticeServiceTest {
         assertThat(response.getContents()).isEqualTo(contents);
         assertThat(response.getUserId()).isEqualTo(userId);
         assertThat(response.getHits()).isEqualTo(hits);
+
+        verify(noticeRepository).findById(any());
+    }
+
+    @Test
+    @DisplayName("공지사항 내림차순 조회 성공")
+    void findAllDesc() {
+        given(noticeRepository.findAllDesc()).willReturn(new ArrayList<>(Arrays.asList(notice)));
+
+        noticeService.findAllDesc();
+
+        verify(noticeRepository).findAllDesc();
     }
 }
